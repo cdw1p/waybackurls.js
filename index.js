39f01be633e7d2a008a459b706a02a832a07e6ae
Cahyo Dwi Putro <17512940+cdw1p@users.noreply.github.com>
2020-09-30 10:47:25 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const { argv } = require('yargs')
+const fetch = require('node-fetch')
+const fs = require('fs')
+
+const getAllData = async (domain) => {
+  let url = `http://web.archive.org/cdx/search/cdx?url=${domain}/*&output=json&collapse=urlkey`
+  return await fetch(url)
+    .then(res => res.json())
+    .then(response => response)
+    .catch(error => error.message)
+}
+
+const checkDomain = (domain) => {
+  if (/^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/.test(domain)) {
+    return true
+  } else {
+    return false
+  }
+}
+
+const writeResult = (data, output) => {
+  data.slice(1).map(items => {
+    fs.appendFileSync(`./${output}`, `${items}\n`)
+  })
+}
+
+(async () => {
+  try {
+    if (argv.d || argv.o) {
+      if (await checkDomain(argv.d)) {
+        const responseData = await getAllData(argv.d)
+        if (responseData.length > 0) {
+          const loopData = responseData.map(data => data[2])
+          await writeResult(loopData, argv.o)
+          console.log(`Process complete! file saved on ${argv.o}`)
+        } else {
+          console.log('No data available.')
+        }
+      } else {
+        console.log('Please enter valid domain.')
+      }
+    } else {
+      console.log('Invalid format usage.\nUsage: node index --d <domain> -o <filename>')
+    }
+  } catch (error) {
+    console.log(error)
+  }
+})()
